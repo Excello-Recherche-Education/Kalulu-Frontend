e2f691b0bb7b57799ca865eb447e0cb921ab6bd3
BimDav <dehaene91@hotmail.com>
2023-11-22 15:18:12 +0100

    working add word

--- sources/language_tool/gp_select_popup.gd
+++ sources/language_tool/gp_select_popup.gd
@@ -1,13 +1,18 @@
 extends Control
 
+signal GP_selected(grapheme_ind: int, text: String)
+signal focus_changed(has_focus: bool)
+
 @onready var container: = $VBoxContainer
+
+
+var grapheme_ind: = -1
+var ind_selected: = -1:
+	set = set_ind_selected
+
 
 func _ready() -> void:
 	top_level = true
-
-
-var ind_selected: = -1:
-	set = set_ind_selected
 
 
 func set_ind_selected(p_ind_selected: int) -> void:
@@ -16,10 +21,16 @@ func set_ind_selected(p_ind_selected: int) -> void:
 		child.set_pressed_no_signal(ind_selected == child.get_index())
 
 
-func clear() -> void:
-	for child in container.get_children():
+func clear(new_grapheme_ind: int) -> void:
+	var children: = container.get_children()
+	if not children.is_empty():
+		if ind_selected == -1:
+			ind_selected = 0
+			GP_selected.emit(grapheme_ind, children[0].text)
+	for child in children:
 		child.queue_free()
 	ind_selected = -1
+	grapheme_ind = new_grapheme_ind
 
 
 func add_item(text: String, selected: bool) -> void:
@@ -29,3 +40,23 @@ func add_item(text: String, selected: bool) -> void:
 	container.add_child(button)
 	if selected:
 		ind_selected = button.get_index()
+	button.toggled.connect(_on_button_toggled.bind(button))
+	button.focus_entered.connect(_on_button_focus_entered)
+	button.focus_exited.connect(_on_button_focus_exited)
+
+
+func _on_button_toggled(pressed: bool, button: CheckBox) -> void:
+	var ind: = button.get_index()
+	if ind_selected != ind:
+		ind_selected = ind
+	if not pressed:
+		button.set_pressed_no_signal(not pressed)
+	GP_selected.emit(grapheme_ind, button.text)
+
+
+func _on_button_focus_entered() -> void:
+	focus_changed.emit(true)
+
+
+func _on_button_focus_exited() -> void:
+	focus_changed.emit(false)
--- sources/language_tool/word_list_element.gd
+++ sources/language_tool/word_list_element.gd
@@ -15,7 +15,14 @@ var graphemes: = "":
 	set = set_graphemes
 var phonemes: = "":
 	set = set_phonemes
-var undo_redo: UndoRedo
+var undo_redo: UndoRedo:
+	get:
+		if not undo_redo:
+			undo_redo = UndoRedo.new()
+		return undo_redo
+var prev_caret_position: int = 0
+var unvalidated_graphemes: = ""
+var unvalidated_phonemes: = ""
 
 
 func set_word(p_word: String) -> void:
@@ -32,11 +39,12 @@ func set_graphemes(p_graphemes: String) -> void:
 		_set_graphemes_label()
 	if graphemes_edit:
 		graphemes_edit.text = graphemes
+	unvalidated_graphemes = graphemes
 
 
 func _set_graphemes_label() -> void:
-	var graphemes_array: = graphemes.split(" ")
-	var phonemes_array: = phonemes.split(" ")
+	var graphemes_array: = graphemes.split(" ", false)
+	var phonemes_array: = phonemes.split(" ", false)
 	graphemes_label.text = ""
 	for i in graphemes_array.size():
 		if i > 0:
@@ -50,6 +58,7 @@ func set_phonemes(p_phonemes: String) -> void:
 	phonemes = p_phonemes
 	if graphemes_label:
 		_set_graphemes_label()
+	unvalidated_phonemes = phonemes
 
 
 func _ready() -> void:
@@ -70,39 +79,84 @@ func _on_minus_button_pressed() -> void:
 
 
 func _on_validate_button_pressed() -> void:
+	popup_menu.clear(-1)
 	undo_redo.create_action("validated")
-	undo_redo.add_do_property(self, "graphemes", graphemes_edit.text)
+	undo_redo.add_do_property(self, "graphemes", unvalidated_graphemes)
+	undo_redo.add_do_property(self, "phonemes", unvalidated_phonemes)
 	undo_redo.add_do_property(self, "word", word_edit.text)
 	undo_redo.add_undo_property(self, "graphemes", graphemes)
+	undo_redo.add_undo_property(self, "phonemes", phonemes)
 	undo_redo.add_undo_property(self, "word", word)
 	undo_redo.commit_action()
 	tab_container.current_tab = 0
 
 
-func _process(_delta: float) -> void:
-	if graphemes_edit.has_focus():
-		var graphemes_array: PackedStringArray = graphemes_edit.text.split(" ")
-		var graphemes_count: = []
-		graphemes_count.resize(graphemes_array.size())
-		var grapheme_ind: = 0
-		for i in graphemes_array.size():
-			if i == 0:
-				graphemes_count[i] = graphemes_array[i].length()
-			else:
-				graphemes_count[i] = graphemes_count[i-1] + 1 + graphemes_array[i].length()
-			if graphemes_count[i] >= graphemes_edit.caret_column:
-				grapheme_ind = i
-				break
-		popup_menu.show()
-		popup_menu.position = graphemes_edit.global_position + Vector2(0, graphemes_edit.size.y)
-		popup_menu.size = Vector2(graphemes_edit.size.x, 0)
-		popup_menu.clear()
-		Database.db.query_with_bindings("Select Grapheme, Phoneme FROM GPs WHERE Grapheme=?", [graphemes_array[grapheme_ind]])
-		for result in Database.db.query_result:
-			popup_menu.add_item(result.Grapheme + "-" + result.Phoneme, false)
-	elif not graphemes_edit.has_focus():
-		popup_menu.hide()
-	else:
-		graphemes_edit.get_window().grab_focus()
+func _get_selected_grapheme(graphemes_array: PackedStringArray) -> int:
+	var graphemes_count: = []
+	graphemes_count.resize(graphemes_array.size())
+	var grapheme_ind: = 0
+	for i in graphemes_array.size():
+		if i == 0:
+			graphemes_count[i] = graphemes_array[i].length()
+		else:
+			graphemes_count[i] = graphemes_count[i-1] + 1 + graphemes_array[i].length()
+		if graphemes_count[i] >= graphemes_edit.caret_column:
+			grapheme_ind = i
+			break
+	return grapheme_ind
 
+
+func _on_popup_gp_selected(ind: int, text: String) -> void:
+	var graphemes_array: PackedStringArray = graphemes_edit.text.split(" ", false)
+	var phonemes_array: PackedStringArray = phonemes.split(" ", false)
+	if ind >= graphemes_array.size():
+		return
+	#var grapheme_ind: = _get_selected_grapheme(graphemes_array)
+	var gp_array: PackedStringArray = text.split("-")
+	graphemes_array[ind] = gp_array[0]
+	if phonemes_array.size() <= ind:
+		phonemes_array.resize(ind + 1)
+	phonemes_array[ind] = gp_array[1]
+	unvalidated_graphemes = " ".join(graphemes_array)
+	unvalidated_phonemes = " ".join(phonemes_array)
+	graphemes_edit.grab_focus()
+	graphemes_edit.caret_column = prev_caret_position
+
+
+func _on_graphemes_edit_gui_input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		return
+	prev_caret_position = graphemes_edit.caret_column
+	var graphemes_array: PackedStringArray = graphemes_edit.text.split(" ", false)
+	if graphemes_array.is_empty():
+		return
+	var phonemes_array: PackedStringArray = phonemes.split(" ", false)
+	var grapheme_ind: int = _get_selected_grapheme(graphemes_array)
+	popup_menu.show()
+	popup_menu.position = graphemes_edit.global_position + Vector2(0, graphemes_edit.size.y)
+	popup_menu.size = Vector2(graphemes_edit.size.x, 0)
+	popup_menu.clear(grapheme_ind)
+	Database.db.query_with_bindings("Select Grapheme, Phoneme FROM GPs WHERE Grapheme=?", [graphemes_array[grapheme_ind]])
+	for result in Database.db.query_result:
+		var is_already_selected: bool = result.Grapheme == graphemes_array[grapheme_ind] and grapheme_ind < phonemes_array.size() and result.Phoneme == phonemes_array[grapheme_ind]
+		popup_menu.add_item(result.Grapheme + "-" + result.Phoneme, is_already_selected)
+
+
+func _on_graphemes_edit_focus_entered() -> void:
+	popup_menu.show()
+
+
+func _on_graphemes_edit_focus_exited() -> void:
+	popup_menu.hide()
+
+
+func _on_popup_focus_changed(p_has_focus: bool) -> void:
+	popup_menu.visible = p_has_focus
+
+
+func _on_graphemes_edit_text_changed(_new_text: String) -> void:
+	var graphemes_array: PackedStringArray = graphemes_edit.text.split(" ", false)
+	var phonemes_array: PackedStringArray = phonemes.split(" ", false)
+	if graphemes_array.size() < phonemes_array.size():
+		phonemes_array.remove_at(popup_menu.grapheme_ind)
 
