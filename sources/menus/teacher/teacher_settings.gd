7774824953aaa422efe754fa5896f7bd5e5e502d
Dylan Sarrazyn <sarrazyndylan@gmail.com>
2024-02-06 09:29:06 +0100

    Adds back button on teacher settings placeholder scene

--- sources/menus/teacher/teacher_settings.gd
+++ sources/menus/teacher/teacher_settings.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_button_pressed():
 	print(UserDataManager.teacher_settings.students)
+
+
+func _on_back_button_pressed():
+	get_tree().change_scene_to_file("res://sources/menus/login/login.tscn")
