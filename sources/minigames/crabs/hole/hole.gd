6e592d34a82d39277a861506f6215506f958f9ee
Dylan Sarrazyn <sarrazyndylan@gmail.com>
2024-03-12 09:34:08 +0100

    Crab scene

--- sources/minigames/crabs/hole/hole.gd
+++ sources/minigames/crabs/hole/hole.gd
@@ -93,7 +93,7 @@ func _on_crab_hit(stimulus: Dictionary) -> void:
 	var tween: = create_tween()
 	tween.tween_property(crab, "position", Vector2(crab_x, -180), 0.5)
 	# TODO FIX
-	#tween.parallel().tween_property(crab.animated_sprite, "rotation_degrees", 540.0, 0.5)
+	tween.parallel().tween_property(crab.animated_sprite, "rotation_degrees", 540.0, 0.5)
 	await tween.finished
 
 	# Make the crab disappear in the hole
