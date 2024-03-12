dc9556547a8678dbbd800a8409ada4830d73598c
Laurent Tourte <77699054+Harpokhrat@users.noreply.github.com>
2024-03-12 17:27:22 +0100

    Added copy to tracing builder

--- sources/language_tool/tracing_builder.gd
+++ sources/language_tool/tracing_builder.gd
@@ -6,6 +6,7 @@ extends Control
 @onready var upper_container: = %UpperContainer
 @onready var letter_picker: = %LetterPicker
 @onready var save_ok: = %SaveOk
+@onready var copy_from: = %CopyFrom
 
 const extension: = ".csv"
 
@@ -14,6 +15,8 @@ var current_letter: = -1
 
 
 func _ready() -> void:
+	copy_from.get_popup().id_pressed.connect(_on_copy_from_id_pressed)
+	
 	lower_container.gradient = gradient
 	upper_container.gradient = gradient
 	
@@ -32,6 +35,7 @@ func _find_all_letters() -> void:
 			if not letter in letters:
 				letters.append(letter)
 				letter_picker.add_item(letter)
+				copy_from.get_popup().add_item(letter)
 
 
 func _next_letter() -> void:
@@ -43,14 +47,8 @@ func _next_letter() -> void:
 	lower_container.grapheme_label.text = letters[current_letter].to_lower()
 	upper_container.grapheme_label.text = letters[current_letter].to_upper()
 	
-	Database.db.query("Select LowerTracingPath, UpperTracingPath FROM LettersTracingData WHERE Letter = '" + letters[current_letter] + "'")
-	
-	if not Database.db.query_result.is_empty():
-		var lower_path: String = Database.db.query_result[0]["LowerTracingPath"]
-		var upper_path: String = Database.db.query_result[0]["UpperTracingPath"]
-		
-		_load_segments(lower_container, lower_path)
-		_load_segments(upper_container, upper_path)
+	_load_segments(lower_container, lower_path(letters[current_letter]))
+	_load_segments(upper_container, upper_path(letters[current_letter]))
 	
 	letter_picker.selected = current_letter
 	save_ok.visible = true
@@ -85,21 +83,21 @@ func _save_segments(segments: Array, path: String) -> void:
 	file.close()
 
 
+func lower_path(letter: String) -> String:
+	return letter + "_lower"
+
+
+func upper_path(letter: String) -> String:
+	return letter + "_upper"
+
+
 func real_path(path: String) -> String:
 	return Database.base_path.path_join(Database.language).path_join(Database.tracing_data_folder).path_join(path) + extension
 
 
 func _on_save_button_pressed() -> void:
-	var lower_path: = letters[current_letter] + "_lower"
-	var upper_path: = letters[current_letter] + "_upper"
-	
-	_save_segments(lower_container.segments_container.get_children(), lower_path)
-	_save_segments(upper_container.segments_container.get_children(), upper_path)
-	
-	Database.db.query("Select LowerTracingPath, UpperTracingPath FROM LettersTracingData WHERE Letter = '" + letters[current_letter] + "'")
-	
-	if Database.db.query_result.is_empty():
-		Database.db.query("INSERT INTO LettersTracingData (Letter, LowerTracingPath, UpperTracingPath) VALUES ('" + letters[current_letter] + "','" + lower_path + "','" + upper_path + "')")
+	_save_segments(lower_container.segments_container.get_children(), lower_path(letters[current_letter]))
+	_save_segments(upper_container.segments_container.get_children(), upper_path(letters[current_letter]))
 	
 	save_ok.visible = true
 
@@ -111,6 +109,11 @@ func _on_back_button_pressed() -> void:
 func _on_letter_picker_item_selected(index: int) -> void:
 	current_letter = index - 1
 	_next_letter()
+
+
+func _on_copy_from_id_pressed(index: int) -> void:
+	_load_segments(lower_container, lower_path(letters[index]))
+	_load_segments(upper_container, upper_path(letters[index]))
 
 
 func _on_lower_container_changed() -> void:
