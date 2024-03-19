fb1a4cfd0c970cdcb8215d4ff241ac805588ceca
BimDav <dehaene91@hotmail.com>
2024-03-19 11:58:45 +0100

    possible to export list of words not found as file

--- sources/language_tool/sentence_list.gd
+++ sources/language_tool/sentence_list.gd
@@ -1,5 +1,9 @@
 extends "res://sources/language_tool/word_list.gd"
 
+var not_found_list: = ""
+@onready var export_not_found_button: = %ExportNotFoundButton
+@onready var file_dialog_export: = $FileDialogExport
+
 
 func create_sub_elements_list() -> void:
 	super()
@@ -41,8 +45,8 @@ func connect_not_found() -> void:
 	for element in elements_container.get_children():
 		if not element.not_found.is_connected(_on_not_found):
 			element.not_found.connect(_on_not_found)
-	if not _e.not_found.is_connected(_on_not_found):
-		_e.not_found.connect(_on_not_found)
+	if not _e.not_found.is_connected(_on_not_found_csv):
+		_e.not_found.connect(_on_not_found_csv)
 
 
 func _on_plus_button_pressed() -> void:
@@ -54,6 +58,10 @@ func _on_not_found(text: String) -> void:
 	error_label.text = text
 
 
+func _on_not_found_csv(text: String) -> void:
+	not_found_list += text + "\n"
+
+
 func _on_list_title_import_path_selected(path: String) -> void:
 	var file: = FileAccess.open(path, FileAccess.READ)
 	var line: = file.get_csv_line()
@@ -61,6 +69,7 @@ func _on_list_title_import_path_selected(path: String) -> void:
 		error_label.text = "Column name should be Sentence"
 		return
 	var inserted_one: = false
+	var not_found_one: = false
 	while not file.eof_reached():
 		line = file.get_csv_line()
 		if line.size() < 1 or line[0] == "":
@@ -73,5 +82,28 @@ func _on_list_title_import_path_selected(path: String) -> void:
 		id = _e._add_from_additional_word_list(line[0])
 		if id >= 0:
 			inserted_one = true
+		else:
+			not_found_one = true
+		
 	if inserted_one:
 		get_tree().reload_current_scene()
+	elif not_found_one:
+		export_not_found_button.show()
+
+
+func _on_export_not_found_button_pressed() -> void:
+	file_dialog_export.filters = []
+	file_dialog_export.add_filter("*.txt", "txt")
+	
+	for connection in file_dialog_export.file_selected.get_connections():
+		connection["signal"].disconnect(connection["callable"])
+	
+	file_dialog_export.file_selected.connect(_on_export_filename_selected)
+	
+	file_dialog_export.show()
+
+
+func _on_export_filename_selected(path: String) -> void:
+	var file: = FileAccess.open(path, FileAccess.WRITE)
+	file.store_string(not_found_list)
+	file.close()
