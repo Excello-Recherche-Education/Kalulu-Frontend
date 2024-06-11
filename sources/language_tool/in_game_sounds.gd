fc45b13d34e148c3469a269e870663499c77ba85
BimDav <dehaene91@hotmail.com>
2024-06-11 14:35:54 +0200

    in prof tool can load sounds for gp

--- sources/language_tool/in_game_sounds.gd
+++ sources/language_tool/in_game_sounds.gd
@@ -4,7 +4,18 @@ extends "res://sources/language_tool/gp_image_and_sound_descriptions.gd"
 
 func _ready():
 	DirAccess.make_dir_recursive_absolute(Database.base_path + Database.language + Database.language_sounds)
-
+	
+	Database.db.query("SELECT * FROM GPs WHERE GPs.Exception=0")
+	for res in Database.db.query_result:
+		var description_line: = description_line_class.instantiate()
+		description_line.get_sound_path = Database.get_gp_sound_path
+		description_line.get_image_path = get_empty_string
+		description_line.hide_image_part()
+		description_container.add_child(description_line)
+		description_line.set_gp(res)
+		description_line.image_preview.hide()
+		description_line.image_upload_button.hide()
+	
 	Database.db.query("SELECT * FROM Syllables WHERE Syllables.Exception=0")
 	for res in Database.db.query_result:
 		var description_line: = description_line_class.instantiate()
