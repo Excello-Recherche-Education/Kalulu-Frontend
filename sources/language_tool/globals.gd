22e481df52df008791118d804444f78be272d218
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-04-09 10:46:57 +0200

    Hard-typing to reduce warnings

--- sources/language_tool/globals.gd
+++ sources/language_tool/globals.gd
@@ -4,6 +4,6 @@ extends Node
 var main_menu_selected_tab: int = 0
 
 func _ready() -> void:
-	var scene_path = get_tree().current_scene.get_scene_file_path()
+	var scene_path: String = get_tree().current_scene.get_scene_file_path()
 	if scene_path != "res://sources/language_tool/prof_tool_menu.tscn":
 		queue_free() # If we are not in the editor, this autoload is not needed since it is only useful for the prof_tool
