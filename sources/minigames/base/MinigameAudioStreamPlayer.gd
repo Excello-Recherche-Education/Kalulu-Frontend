778781e0ac6e4e4014d42fcbacddc75cfc3543b8
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-04-30 16:21:05 +0200

    Debug audio player

--- sources/minigames/base/MinigameAudioStreamPlayer.gd
+++ sources/minigames/base/MinigameAudioStreamPlayer.gd
@@ -6,7 +6,7 @@ func play_gp(gp: Dictionary) -> void:
 	if not gp or gp.is_empty():
 		return
 	
-	var phoneme_audiostream: AudioStream = Database.load_external_sound(Database.get_gp_sound_path(gp)) as AudioStream
+	var phoneme_audiostream: AudioStreamMP3 = Database.load_external_sound(Database.get_gp_sound_path(gp)) as AudioStream
 	if not phoneme_audiostream:
 		Logger.warn("MinigameAudioStreamPlayer: AudioStream not found for gp %s " % gp)
 		return
@@ -18,7 +18,7 @@ func play_syllable(syllable: Dictionary) -> void:
 	if not syllable or syllable.is_empty():
 		return
 	
-	var syllable_audiostream: AudioStream = Database.load_external_sound(Database.get_syllable_sound_path(syllable))
+	var syllable_audiostream: AudioStreamMP3 = Database.load_external_sound(Database.get_syllable_sound_path(syllable))
 	if not syllable_audiostream:
 		Logger.warn("MinigameAudioStreamPlayer: AudioStream not found for syllable %s " % syllable)
 		return
@@ -30,15 +30,15 @@ func play_word(word: String) -> void:
 	if not word or word.is_empty():
 		return
 	
-	var word_audiostream: AudioStream = Database.load_external_sound(Database.get_word_sound_path({Word = word}))
+	var word_audiostream: AudioStreamMP3 = Database.load_external_sound(Database.get_word_sound_path({Word = word}))
 	if not word_audiostream:
 		Logger.warn("MinigameAudioStreamPlayer: AudioStream not found for word %s " % word)
 		return
 	
 	await play_audio_stream(word_audiostream)
 
-func play_audio_stream(audio: AudioStream) -> void:
+func play_audio_stream(audio: AudioStreamMP3) -> void:
 	stream = audio
 	play()
-	if playing:
-		await finished
+	if not audio.loop:
+		await get_tree().create_timer(audio.get_length() + 0.25).timeout
