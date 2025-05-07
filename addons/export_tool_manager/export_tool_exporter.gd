ef9b138788eace650a4cb51e11df1bf7dc9dcba9
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-05-07 15:47:19 +0200

    Export tool v2

--- addons/export_tool_manager/export_tool_exporter.gd
+++ addons/export_tool_manager/export_tool_exporter.gd
@@ -2,7 +2,7 @@
 extends EditorExportPlugin
 
 func _get_name() -> String:
-	return "KaluluExportToolExporter"  # ou le nom que tu veux
+	return "KaluluExportToolExporter"
 
 var tool_configs: Dictionary = {
 	"game": {
@@ -16,7 +16,6 @@ var tool_configs: Dictionary = {
 }
 
 func _export_begin(features: PackedStringArray, is_debug: bool, path: String, flags: int) -> void:
-	# Récupère la sélection depuis EditorSettings dynamiquement
 	var editor_settings: EditorSettings = EditorPlugin.new().get_editor_interface().get_editor_settings()
 	var selected_tool: String = "game"
 
@@ -28,102 +27,5 @@ func _export_begin(features: PackedStringArray, is_debug: bool, path: String, fl
 		return
 
 	var config = tool_configs[selected_tool]
-
-	# 1. Met à jour la scène principale
 	ProjectSettings.set_setting("application/run/main_scene", config["main_scene"])
 	ProjectSettings.save()
-
-	if selected_tool == "game":
-		print("[Export Tool Plugin] Export mode = game: nothing will be changed")
-		return
-	elif selected_tool == "prof_tool":
-		print("[Export Tool Plugin] Export mode = prof_tool: plugin will rename the exported file")
-		call_deferred("_postprocess_dmg", path, config["output_name"])
-	else:
-		push_error("[Export Tool Plugin] Export mode = %s: This is not a known mode, nothing will be changed" % selected_tool)
-		return
-
-
-func _wait_for_file(path: String, timeout_sec: float) -> void:
-	var time: float = 0.0
-	while not FileAccess.file_exists(path) and time < timeout_sec:
-		await Engine.get_main_loop().process_frame
-		time += 1.0 / ProjectSettings.get_setting("physics/common/physics_fps", 60)
-
-
-func _postprocess_dmg(dmg_path: String, new_app_name: String) -> void:
-	await _wait_for_file(dmg_path, 25.0)
-	
-	var dmg_dir: String = dmg_path.get_base_dir()
-	var temp_dir: String = dmg_dir.path_join("temp_export_dmg")
-	var temp_app_name: String = "%s.app" % new_app_name
-	var temp_dmg_path: String = dmg_dir.path_join("%s.dmg" % new_app_name)
-
-	# 1. Créer dossier temporaire
-	DirAccess.make_dir_recursive_absolute(temp_dir)
-
-	# 2. Monter l'ancien .dmg
-	var mount_point: String = "/Volumes/KALULU_TEMP"
-	var output: Array = []
-	var mount_result = OS.execute("hdiutil", ["attach", dmg_path, "-mountpoint", mount_point, "-nobrowse", "-readonly"], output)
-	if mount_result != 0:
-		push_error("Échec du montage du .dmg : " + str(output))
-		return
-
-	# 3. Copier l'app depuis le dmg dans temp_dir
-	var original_app_path: String = mount_point.path_join("kalulu.app")
-	output.clear()
-	var copy_result: int = OS.execute("cp", ["-R", original_app_path, temp_dir.path_join(temp_app_name)], output)
-	if copy_result != 0:
-		push_error("Échec de la copie de l'app : " + str(output))
-		return
-
-	# 4. Démonter le .dmg original
-	OS.execute("hdiutil", ["detach", mount_point], output)
-
-	# 5. Recréer un .dmg avec le bon nom
-	output.clear()
-	var create_result = OS.execute("hdiutil", [
-		"create",
-		"-volname", new_app_name,
-		"-srcfolder", temp_dir,
-		"-ov",
-		"-format", "UDZO",
-		temp_dmg_path
-	], output)
-
-	if create_result != 0:
-		push_error("Échec de la création du nouveau .dmg : " + str(output))
-		return
-
-	# 6. Nettoyage et renommage final
-	DirAccess.remove_absolute(dmg_path)
-
-	var final_dmg_path := dmg_dir.path_join("%s.dmg" % new_app_name)
-	var rename_result := DirAccess.rename_absolute(temp_dmg_path, final_dmg_path)
-	if rename_result != OK:
-		push_error("Échec du renommage final du .dmg")
-
-	_cleanup_mounted_volumes(new_app_name)
-	OS.move_to_trash(temp_dir)
-
-
-func _cleanup_mounted_volumes(volume_name: String) -> void:
-	var output := []
-	var result := OS.execute("hdiutil", ["info"], output)
-	if result != 0:
-		push_warning("Impossible de récupérer la liste des volumes montés")
-		return
-
-	if output.size() == 0:
-		return
-
-	var lines: PackedStringArray = output[0].split("\n")
-	for line in lines:
-		if line.find(volume_name) != -1:
-			var match := line.strip_edges()
-			var path := match.split("\t")[-1]
-			if path.begins_with("/Volumes/"):
-				var detach_result := OS.execute("hdiutil", ["detach", path])
-				if detach_result != 0:
-					push_warning("Échec du démontage de : " + path)
--- addons/export_tool_manager/export_tool_manager_plugin.gd
+++ addons/export_tool_manager/export_tool_manager_plugin.gd
@@ -5,14 +5,15 @@ const EXPORTER_PATH: String = "res://addons/export_tool_manager/export_tool_expo
 
 var tool_selector: OptionButton
 var exporter_plugin: EditorExportPlugin
+var export_button: Button
 
 func _enter_tree() -> void:
+	# Tool Selector UI
 	tool_selector = OptionButton.new()
 	tool_selector.name = "Tool Exporter"
 	tool_selector.add_item("Game")
 	tool_selector.add_item("Prof Tool")
 	tool_selector.connect("item_selected", _on_tool_selected)
-
 	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, tool_selector)
 
 	var settings: EditorSettings = get_editor_interface().get_editor_settings()
@@ -28,19 +29,64 @@ func _enter_tree() -> void:
 		"game", _:
 			tool_selector.select(0)
 
-	# On instancie le plugin d'export
+	# Exporter plugin
 	exporter_plugin = load(EXPORTER_PATH).new()
 	add_export_plugin(exporter_plugin)
 
+	# Export All Button
+	export_button = Button.new()
+	export_button.text = "Export All (Game)"
+	export_button.pressed.connect(_on_export_all_game_pressed)
+	add_control_to_container(EditorPlugin.CONTAINER_TOOLBAR, export_button)
+
 func _exit_tree() -> void:
 	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, tool_selector)
-	if exporter_plugin:
-		remove_export_plugin(exporter_plugin)
+	tool_selector.queue_free()
+
+	remove_control_from_container(EditorPlugin.CONTAINER_TOOLBAR, export_button)
+	export_button.queue_free()
+
+	remove_export_plugin(exporter_plugin)
 
 func _on_tool_selected(index: int) -> void:
-	var selected_tool := tool_selector.get_item_text(index).to_lower().replace(" ", "_")
-	if selected_tool == "":
-		selected_tool = "game"
+	var tool := "game"
+	match index:
+		1:
+			tool = "prof_tool"
+		0, _:
+			tool = "game"
 
 	var settings := get_editor_interface().get_editor_settings()
-	settings.set_setting("export_tool_manager/current_tool", selected_tool)
+	settings.set_setting("export_tool_manager/current_tool", tool)
+
+func _on_export_all_game_pressed():
+	export_all_game_presets()
+
+func export_all_game_presets():
+	var godot_path := OS.get_executable_path()
+	var presets := {
+		"Android Kalulu AAB": "../Export/autobuilds/Android/kalulu_app.aab",
+		"Android Kalulu APK": "../Export/autobuilds/Android/kalulu_app.apk",
+		"Windows Kalulu": "../Export/autobuilds/Windows/Kalulu.exe",
+		"Linux Kalulu": "../Export/autobuilds/Linux/Kalulu.x86_64",
+		# Apple in last because it's always the most complicated
+		"iOS Kalulu": "../Export/autobuilds/iOS/KaluluApp.ipa",
+		"macOS Kalulu": "../Export/autobuilds/macOS/Kalulu.dmg"
+	}
+
+	for preset_name in presets.keys():
+		await get_tree().create_timer(1).timeout
+		var output_path: String = presets[preset_name]
+		DirAccess.make_dir_recursive_absolute(output_path.get_base_dir())
+
+		var args := ["--headless", "--export-release", preset_name, output_path]
+		var output := []
+		var result := OS.execute(godot_path, args, output, true)
+
+		if result != OK:
+			push_error("Export failed for %s (%s)\nOutput:\n%s" % [
+				preset_name, output_path, "\n".join(PackedStringArray(output))
+			])
+			return
+		else:
+			print("✔ Export success : %s → %s" % [preset_name, output_path])
