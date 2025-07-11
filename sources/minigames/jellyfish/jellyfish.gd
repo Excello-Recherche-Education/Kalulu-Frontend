87b1d4678d11cacc4db51c242cfba616a82b6379
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-07-11 15:39:00 +0200

    Import new assets and add text box for jellyfish

--- sources/minigames/jellyfish/jellyfish.gd
+++ sources/minigames/jellyfish/jellyfish.gd
@@ -9,9 +9,13 @@ enum Colors {
 	Green,
 }
 
-const ANIMATIONS: Array[SpriteFrames] = [
-	preload("res://sources/minigames/jellyfish/red_jellyfish_animations.tres"),
-	preload("res://sources/minigames/jellyfish/green_jellyfish_animations.tres"),
+const ANIMATIONS_BODY: Array[SpriteFrames] = [
+	preload("res://sources/minigames/jellyfish/blue_jellyfish_animations_body.tres"),
+	preload("res://sources/minigames/jellyfish/pink_jellyfish_animations_body.tres"),
+]
+const ANIMATIONS_ARMS: Array[SpriteFrames] = [
+	preload("res://sources/minigames/jellyfish/blue_jellyfish_animations_arms.tres"),
+	preload("res://sources/minigames/jellyfish/pink_jellyfish_animations_arms.tres"),
 ]
 
 const SCALES: Array[Vector2] = [
@@ -24,19 +28,29 @@ const SCALE_FACTOR: float = 0.2
 @export var color: int = Colors.Red:
 	set(value):
 		color = value
-		if animated_sprite:
-			animated_sprite.sprite_frames = ANIMATIONS[color]
+		if animated_sprite_body:
+			animated_sprite_body.sprite_frames = ANIMATIONS_BODY[color]
+		else:
+			Logger.error("Jellyfish: no animated sprite body")
+		if animated_sprite_arms:
+			animated_sprite_arms.sprite_frames = ANIMATIONS_ARMS[color]
+		else:
+			Logger.error("Jellyfish: no animated sprite arms")
 		scale = SCALES[color] * (1. + randf() * SCALE_FACTOR)
 		
 		# Handles sprite size
 		sprite_control.resized.emit()
 
 @onready var sprite_control: SpriteControl = $SpriteControl
-@onready var animated_sprite: AnimatedSprite2D = %AnimatedSprite2D
+@onready var animated_sprite_body: AnimatedSprite2D = %AnimatedSprite2D_Body
+@onready var animated_sprite_arms: AnimatedSprite2D = %AnimatedSprite2D_Arms
 @onready var label: Label = %AutoSizeLabel.get_node("Label")
 @onready var highlight_fx: HighlightFX = %HighlightFX
 @onready var right_fx: RightFX = %RightFX
 @onready var wrong_fx: WrongFX = %WrongFX
+@onready var text_box_sprite_2d: Sprite2D = %TextBox_Sprite2D
+@onready var text_box_outline_sprite_2d: Sprite2D = %TextBox_Outline_Sprite2D
+
 
 var stimulus: Dictionary = {}:
 	set(value):
@@ -53,24 +67,31 @@ func _ready() -> void:
 	
 	var rand: float = randf()
 	color = Colors.Red if rand < 0.7 else Colors.Green
-	animated_sprite.play("idle")
-	animated_sprite.frame = randi_range(0, animated_sprite.sprite_frames.get_frame_count("idle") - 1)
+	var rand_frame: int = randi_range(0, animated_sprite_body.sprite_frames.get_frame_count("idle") - 1)
+	animated_sprite_body.frame = rand_frame
+	animated_sprite_arms.frame = rand_frame
+	idle()
 
 
 func is_idle() -> bool:
-	return animated_sprite.animation == "idle"
+	return animated_sprite_body.animation == "idle"
 
 
 func happy() -> void:
-	animated_sprite.play("happy")
+	play_animation_by_name("happy")
 
 
 func idle() -> void:
-	animated_sprite.play("idle")
+	play_animation_by_name("idle")
 
 
 func hit() -> void:
-	animated_sprite.play("hit")
+	play_animation_by_name("hit")
+
+
+func play_animation_by_name(animation_name: String) -> void:
+	animated_sprite_body.play(animation_name)
+	animated_sprite_arms.play(animation_name)
 
 
 func highlight() -> void:
@@ -82,11 +103,21 @@ func stop_highlight() -> void:
 
 
 func right() -> void:
+	label.label_settings = label.label_settings.duplicate()
+	label.label_settings.font_color = Color("#009444")
+	text_box_sprite_2d.self_modulate = Color("#e6f3e0")
+	text_box_outline_sprite_2d.self_modulate = Color("#009344")
+	text_box_outline_sprite_2d.visible = true
 	right_fx.play()
 	await right_fx.finished
 
 
 func wrong() -> void:
+	label.label_settings = label.label_settings.duplicate()
+	label.label_settings.font_color = Color("#be1e2d")
+	text_box_sprite_2d.self_modulate = Color("#fce6e6")
+	text_box_outline_sprite_2d.self_modulate = Color("#be1e2d")
+	text_box_outline_sprite_2d.visible = true
 	wrong_fx.play()
 	await wrong_fx.finished
 
