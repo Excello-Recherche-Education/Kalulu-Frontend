1508f9e3046b1f87882ad39e805ee48f55d95942
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-09-23 15:27:34 +0200

    Add log level selector

--- sources/ui/log_viewer.gd
+++ sources/ui/log_viewer.gd
@@ -4,11 +4,20 @@ extends AcceptDialog
 @onready var log_text: TextEdit = $VBox/LogText
 @onready var slider: HSlider = $VBox/Controls/LineCountSlider
 @onready var slider_label: Label = $VBox/Controls/SliderLabel
+@onready var level_dropdown: OptionButton = $VBox/Controls/LogLevelDropdown
 
 var file_path: String
 var all_lines: PackedStringArray = []
 var line_steps: PackedInt32Array = [10, 50, 100, 200, 500, 1000, -1] # -1 = all
 
+func _ready() -> void:
+	# Fill the list with the log enum
+	if level_dropdown.get_item_count() == 0:
+		for level_name: String in Log.LogLevel.keys():
+			var value: int = Log.LogLevel[level_name]
+			level_dropdown.add_item(level_name, value)
+		
+		level_dropdown.item_selected.connect(_on_level_changed)
 
 func show_log_file(path: String) -> void:
 	file_path = path
@@ -34,13 +43,14 @@ func show_log_file(path: String) -> void:
 	if not slider.value_changed.is_connected(_on_slider_changed):
 		slider.value_changed.connect(_on_slider_changed)
 	
+	level_dropdown.select(Log.current_level)
+	
 	_update_log_text()
 	popup_centered_ratio(0.9)
 
 
 func _on_slider_changed(_value: float) -> void:
 	_update_log_text()
-
 
 func _update_log_text() -> void:
 	var idx: int = int(slider.value)
@@ -58,3 +68,8 @@ func _update_log_text() -> void:
 	
 	log_text.text = "\n".join(subset)
 	log_text.scroll_vertical = log_text.get_line_count() # Scroll down
+
+func _on_level_changed(_index: int) -> void:
+	var selected_level: int = level_dropdown.get_selected_id()
+	Log.current_level = selected_level as Log.LogLevel
+	Log.info("Log level changed to %s" % Log.LogLevel.keys()[selected_level])
