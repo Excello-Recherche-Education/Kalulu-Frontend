7db108498fea58c2ac822aa37ede8385f6515a24
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-09-30 16:52:40 +0200

    Add logs at startup

--- sources/menus/splash_screen/splash_screen.gd
+++ sources/menus/splash_screen/splash_screen.gd
@@ -4,6 +4,7 @@ const MAIN_MENU_SCENE_PATH: String = "res://sources/menus/main/main_menu.tscn"
 
 
 func _go_to_main_menu() -> void:
+	Log.info("SplashScreen: Changing scene to main menu")
 	var err: Error = get_tree().change_scene_to_file(MAIN_MENU_SCENE_PATH)
 	if err != 0:
 		Log.error("SplashScreen: Error while going to main menu: " + str(err))
@@ -16,3 +17,7 @@ func _on_timer_timeout() -> void:
 func _on_gui_input(event: InputEvent) -> void:
 	if event.is_action_pressed("left_click"):
 		_go_to_main_menu()
+
+
+func _on_timer_ready() -> void:
+	Log.info("SplashScreen: Starting timer")
