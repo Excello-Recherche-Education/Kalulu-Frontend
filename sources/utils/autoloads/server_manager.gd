24899ee41a7d3a8e5a3c69cb184237b43e703aa8
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2025-12-04 13:35:30 +0100

    New API Gateway Configuration

--- sources/utils/autoloads/server_manager.gd
+++ sources/utils/autoloads/server_manager.gd
@@ -5,8 +5,7 @@ signal request_completed(success: bool, code: int, body: Dictionary)
 signal internet_check_completed(has_access: bool)
 
 const INTERNET_CHECK_URL: String = "https://google.com"
-const AWS_API_GATEWAY_ADRESS: String = "https://xwvmrarnb7.execute-api.eu-west-3.amazonaws.com"
-const AWS_PASSWORD_RESET_ADRESS: String = "https://10g8bfw6c3.execute-api.eu-west-3.amazonaws.com/dev/"
+const AWS_API_GATEWAY_ADRESS: String = "https://api.kalulu.org"
 
 # Response from the last request
 var success: bool
@@ -95,10 +94,7 @@ func login(mail: String, password: String) -> Dictionary:
 
 
 func reset_password(mail: String) -> Dictionary:
-	var previous: String = environment_url
-	environment_url = AWS_PASSWORD_RESET_ADRESS
 	await _post_json_request("forgot", {"email": mail})
-	environment_url = previous
 	return _response()
 
 
@@ -220,6 +216,7 @@ func _get_request(uri: String, params: Dictionary) -> void:
 func _post_request(uri: String, params: Dictionary) -> void:
 	reset_result()
 	var url: String = _create_uri_with_parameters(environment_url + uri, params)
+	Log.debug(url)
 	var headers: PackedStringArray = _create_request_headers()
 	if params.has("password"):
 		Log.trace("ServerManager: Sending POST request.\n    URI = %s\n    Parameters not logged because it contains a password." % uri)
@@ -236,6 +233,7 @@ func _post_request(uri: String, params: Dictionary) -> void:
 func _post_json_request(uri: String, data: Dictionary) -> void:
 	reset_result()
 	var req: String = environment_url + uri
+	Log.debug(req)
 	var headers: PackedStringArray = _create_request_headers(true)
 	if data.has("password"):
 		Log.trace("ServerManager: Sending POST JSON request.\n    URI = %s\n    Data not logged because it contains a password." % uri)
@@ -252,6 +250,7 @@ func _post_json_request(uri: String, data: Dictionary) -> void:
 func _delete_request(uri: String, params: Dictionary = {}) -> void:
 	reset_result()
 	var req: String = _create_uri_with_parameters(environment_url + uri, params)
+	Log.debug(req)
 	var headers: PackedStringArray = _create_request_headers()
 	Log.trace("ServerManager: Sending DELETE request.\n    URI = %s\n    Parameters = %s" % [uri, params])
 	if http_request.request(req, headers, HTTPClient.METHOD_DELETE, "") == OK:
