5a049dfdaa00c38a9f0626eaf84674b8bf8a3410
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2026-01-20 16:47:48 +0100

    Add label under the lilypad

--- sources/minigames/frog/lilypad.gd
+++ sources/minigames/frog/lilypad.gd
@@ -13,14 +13,28 @@ var top_to_bottom: bool = false
 
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 @onready var button: TextureButton = $TextureButton
-@onready var label: Label = %Label
+@onready var label: Label = $TextureButton/TextBox_Sprite2D/PercentMarginContainer/AspectRatioContainer/AutoSizeLabel/Label
 @onready var highlight_fx: HighlightFX = %HighlightFX
 @onready var right_fx: RightFX = %RightFX
 @onready var wrong_fx: WrongFX = %WrongFX
+@onready var text_box_sprite_2d: Sprite2D = %TextBox_Sprite2D
 
 
 func disappear() -> void:
 	button.set_disabled(true)
+	var tween: Tween = create_tween()
+	tween.parallel().tween_property(
+		label,
+		"self_modulate:a",
+		0.0,
+		0.5
+	)
+	tween.parallel().tween_property(
+		text_box_sprite_2d,
+		"self_modulate:a",
+		0.0,
+		0.5
+	)
 	animation_player.play("disappear")
 
 
@@ -30,11 +44,17 @@ func highlight() -> void:
 
 
 func right() -> void:
+	label.label_settings = label.label_settings.duplicate()
+	label.label_settings.font_color = Minigame.LABEL_COLOR_NEUTRAL
+	text_box_sprite_2d.self_modulate = Minigame.LABEL_COLOR_WIN
 	right_fx.play()
 	await right_fx.finished
 
 
 func wrong() -> void:
+	label.label_settings = label.label_settings.duplicate()
+	label.label_settings.font_color = Minigame.LABEL_COLOR_NEUTRAL
+	text_box_sprite_2d.self_modulate = Minigame.LABEL_COLOR_LOSE
 	wrong_fx.play()
 	await wrong_fx.finished
 
