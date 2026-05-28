09efa6bf5f5e6ce5ef2da514b43bfa3b021a76b1
Adrien Ufferte <adrien.ufferte@netcourrier.com>
2026-05-28 12:05:30 +0200

    Debug boss button texture

--- sources/gardens/boss_button.gd
+++ sources/gardens/boss_button.gd
@@ -3,6 +3,8 @@ extends LessonButton
 
 @export var icon_texture: Texture2D:
 	set = _set_icon_texture
+@export var completed_icon_texture: Texture2D:
+	set = _set_completed_icon_texture
 
 @onready var icon: TextureRect = %Icon
 
@@ -11,10 +13,28 @@ func _ready() -> void:
 	super()
 	if label:
 		label.hide()
-	_set_icon_texture(icon_texture)
+	_refresh_icon()
 
 
 func _set_icon_texture(value: Texture2D) -> void:
 	icon_texture = value
-	if icon:
-		icon.texture = value
+	_refresh_icon()
+
+
+func _set_completed_icon_texture(value: Texture2D) -> void:
+	completed_icon_texture = value
+	_refresh_icon()
+
+
+func _set_completed(value: bool) -> void:
+	super(value)
+	_refresh_icon()
+
+
+func _refresh_icon() -> void:
+	if not icon:
+		return
+	if completed and completed_icon_texture:
+		icon.texture = completed_icon_texture
+	else:
+		icon.texture = icon_texture
